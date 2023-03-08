e337df9dd4d6aa59ffec6ef607e76442c909d3f0
Chrispinetot <47722402+Chrispinetot@users.noreply.github.com>
2023-03-08 22:25:08 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-# define dash apps and it's attributes
-
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE],
-                meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-                suppress_callback_exceptions=True
-                )
